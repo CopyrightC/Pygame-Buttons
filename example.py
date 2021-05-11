ee1fa563ff6a13481bf299882a8b7c87ba47ae31
Anonymous_Programmer <68592417+SATAN01@users.noreply.github.com>
2021-05-11 20:35:00 +0530

    Create example.py

---
+++ example.py
@@ -0,0 +1,30 @@
+import pygame
+from pygamebtn import Button #Import the Button class
+ 
+import sys
+pygame.init()
+
+#Button function
+def x():
+    print("Button pressed")
+
+#Pygame window
+window = pygame.display.set_mode((1000,600))
+pygame.display.set_caption("Pygamebtn")
+
+#Button object ; Note this has to be out of the while loop!
+
+button1 = Button(width = 140,height = 40,color =(30,30,30),hover_color = (29,49,80) )
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONUP:  #This is important!
+            button1.pressed = False # buttonx.pressed = False
+            
+    button1.place(window,x=420,y=300) #x,y coordinate of button
+    button1.text(window,text="Click Me!",x=450,y=312,color=(155,110,124),size =16) #Text
+    button1.function(x) #Execute function x upon clicking
+    pygame.display.update()
